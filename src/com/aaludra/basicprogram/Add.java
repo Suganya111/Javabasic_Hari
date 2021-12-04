df07d93329de43c3aa5170306d3258818be8fbe5
Harivishnu55 <harivishnu2741@gmail.com>
2021-12-04 18:51:34 +0530

    First

--- src/com/aaludra/basicprogram/Add.java
+++ src/com/aaludra/basicprogram/Add.java
@@ -1,11 +1,11 @@
-package com.aaludra.basic;
+package com.aaludra.basicprogram;
 
-public class Addition {
+public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
-
+int i=2;
+System.out.println(i);
 	}
 
 }
