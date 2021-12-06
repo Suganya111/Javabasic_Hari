6e93c98726342483471c0db50d97d6eab2db3f63
Harivishnu55 <harivishnu2741@gmail.com>
2021-12-06 17:57:44 +0530

    New while loop program added

---
+++ src/com/aaludra/basicprogram/Whileloop.java
@@ -0,0 +1,15 @@
+package com.aaludra.basicprogram;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int i= 9;
+		int j= i+1;
+		while(j<=40) {
+			System.out.println(j);
+			j++;
+		}
+
+	}
+
+}
