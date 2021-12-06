71272fb2e61f0c39bb3c5d2139407611cc1bdf80
Harivishnu55 <harivishnu2741@gmail.com>
2021-12-06 18:12:05 +0530

    New forloop program

---
+++ src/com/aaludra/basicprogram/Forloop.java
@@ -0,0 +1,19 @@
+package com.aaludra.basicprogram;
+
+import java.util.Scanner;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+	
+		
+		for(int i=0; i< text.length() ;i++) {
+			System.out.println(text.charAt(i));
+			
+			
+		}
+	}
+
+}
--- src/com/aaludra/basicprogram/Whileloop.java
+++ src/com/aaludra/basicprogram/Whileloop.java
@@ -5,7 +5,9 @@ public class Whileloop {
 	public static void main(String[] args) {
 		int i= 9;
 		int j= i+1;
+		
 		while(j<=40) {
+			
 			System.out.println(j);
 			j++;
 		}
