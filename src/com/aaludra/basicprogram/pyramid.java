fe888940d38901d7b412e0045dca2d4606fbcae7
Harivishnu55 <harivishnu2741@gmail.com>
2021-12-07 12:37:54 +0530

    New pyramid program added

---
+++ src/com/aaludra/basicprogram/pyramid.java
@@ -0,0 +1,28 @@
+package com.aaludra.basicprogram;
+
+import java.util.Scanner;
+
+public class pyramid {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the value to be printed");
+		int a=sc.nextInt();
+		
+		for(int i=0;i<=a;i++)
+		{
+		for(int j=0;j<i;j++)
+		{
+		System.out.print(" " + i);
+
+
+		}
+		System.out.println(" ");
+		sc.close();
+		}
+		System.out.println("");
+		System.out.println("End of triangle ");
+
+	}
+
+}
