2596683b1cbcc83f2401bbbb276fef9e40e0c526
Harivishnu55 <harivishnu2741@gmail.com>
2021-12-11 18:19:29 +0530

    changed

--- src/com/aaludra/basicprogram/JavaConstructors.java
+++ src/com/aaludra/basicprogram/JavaConstructors.java
@@ -1,8 +1,6 @@
 package com.aaludra.basicprogram;
 
 public class JavaConstructors {
-	int a = 3;
-	int b = 4;
 
 	public JavaConstructors(String s, int i) {
 		int c = i;
@@ -13,7 +11,7 @@ public class JavaConstructors {
 
 	public static void main(String[] args) {
 
-		JavaConstructors obj = new JavaConstructors("welcome to java constructor", 5);
+		new JavaConstructors("welcome to java constructor", 5);
 
 	}
 
---
+++ src/com/aaludra/basicprogram/Polymorphism.java
@@ -0,0 +1,14 @@
+package com.aaludra.basicprogram;
+
+public class Polymorphism {
+	class earth {
+		public void mountains(String s) {
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
